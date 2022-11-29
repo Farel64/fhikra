5e05f5758546907d21fce3ecc4ad9fc3dc727af4
Farel64 <119279434+Farel64@users.noreply.github.com>
2022-11-29 15:34:49 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -12,20 +12,18 @@ let handler = async (m, { conn, usedPrefix }) => {
   let vn = `https://github.com/saipulanuar/Api-Github/raw/main/audio/Donasiku.mp3`
   conn.sendButton(m.chat, `
 ┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
-┃✾ Gopay  [088279268363]
-┃✾ Dana  [088279268363]
-┃✾ Saweria  [https://saweria.co/raraharsita2]
+┃✾ Gopay  ---
+┃✾ Dana  ----
+┃✾ Saweria  ----
 ┗━━━━ꕥ
 ┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
-┃ Ingin donasi? Wa.me/6288279268363
-┃ _Hasil donasi akan digunakan buat sewa_
-┃ _atau beli *RDP/VPS* agar bot bisa jalan_
-┃ _24jam tanpa kendala_
+┃ Terimahkasih atas niat anda
+┃ yg ingin berdonasi!!!
 ┗━━━━ꕥ
 `.trim(), wm, [['Owner',`/owner`]],m) // Tambah sendiri kalo mau
 conn.sendFile(m.chat, vn, 'haori.mp3', null, m, true, {
 type: 'audioMessage', 
-ptt: true, contextInfo:{ externalAdReply: { title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`, body: `Follow Tiktok My Bestie`, sourceUrl: 'https://www.tiktok.com/@raraharsita2', thumbnail: await (await fetch('https://i.ibb.co/jfZVKmC/babi2.jpg')).buffer(),}} 
+ptt: true, contextInfo:{ externalAdReply: { title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`, body: `my facebook`, sourceUrl: 'https://www.facebook.com/Farel.Arel15', thumbnail: await (await fetch('https://telegra.ph/file/ea9706a8ef6d102bb819a.jpg')).buffer(),}} 
      }) 
 }
 handler.command = /^(donasi|dns)$/i
